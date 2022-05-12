e2c8b6909fdf5495c817310b1a05567b4b9b1893
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2022-05-12 22:21:41 +0200

    22w19a, removed mixin.ai.poi optimizations.

--- src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
+++ src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
@@ -35,9 +35,9 @@ public class LithiumConfig {
         this.addMixinRule("ai.nearby_entity_tracking", true);
         this.addMixinRule("ai.nearby_entity_tracking.goals", true);
         this.addMixinRule("ai.pathing", true);
-        this.addMixinRule("ai.poi", true);
-        this.addMixinRule("ai.poi.fast_portals", true);
-        this.addMixinRule("ai.poi.poi.tasks", true);
+        this.addMixinRule("ai.poi", false); //removed during 22w19a
+        this.addMixinRule("ai.poi.fast_portals", false); //removed during 22w19a
+        this.addMixinRule("ai.poi.poi.tasks", false); //removed during 22w19a
         this.addMixinRule("ai.raid", true);
         this.addMixinRule("ai.sensor", true);
         this.addMixinRule("ai.sensor.secondary_poi", true);
