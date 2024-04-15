e70717ee3fa9116b129bd420b732a3bf4f946182
Drex <nicknamedrex@gmail.com>
2024-04-15 22:51:30 +0200

    Fix step height entity collisions

--- lithium-mixin-config.md
+++ lithium-mixin-config.md
@@ -250,7 +250,7 @@ Requirements:
   
 ### `mixin.entity.collisions.movement`
 (default: `true`)  
-Entity movement uses optimized block access and optimized and delayed entity access  
+Entity movement uses optimized block/entity access  
 Requirements:
 - `mixin.util.chunk_access=true`  
   
--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/movement/EntityMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/movement/EntityMixin.java
@@ -25,8 +25,8 @@ public class EntityMixin {
                     target = "Lnet/minecraft/world/World;getEntityCollisions(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Box;)Ljava/util/List;"
             )
     )
-    private List<VoxelShape> getEntitiesLater(World world, Entity entity, Box box) {
-        return List.of();
+    private List<VoxelShape> getEntitiesOptimized(World world, Entity entity, Box movementSpace) {
+        return LithiumEntityCollisions.getEntityWorldBorderCollisions(world, entity, movementSpace, entity != null);
     }
 
 
@@ -37,8 +37,8 @@ public class EntityMixin {
                     target = "Lnet/minecraft/entity/Entity;adjustMovementForCollisions(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Lnet/minecraft/world/World;Ljava/util/List;)Lnet/minecraft/util/math/Vec3d;"
             )
     )
-    private Vec3d adjustMovementForCollisionsGetEntitiesLater(@Nullable Entity entity, Vec3d movement, Box entityBoundingBox, World world, List<VoxelShape> collisions) {
-        return lithiumCollideMultiAxisMovement(entity, movement, entityBoundingBox, world, true, collisions);
+    private Vec3d adjustMovementForCollisionsOptimized(@Nullable Entity entity, Vec3d movement, Box entityBoundingBox, World world, List<VoxelShape> collisions) {
+        return lithiumCollideMultiAxisMovement(entity, movement, entityBoundingBox, world, collisions);
     }
 
     /**
@@ -47,10 +47,10 @@ public class EntityMixin {
      */
     @Overwrite
     public static Vec3d adjustMovementForCollisions(@Nullable Entity entity, Vec3d movement, Box entityBoundingBox, World world, List<VoxelShape> collisions) {
-        return lithiumCollideMultiAxisMovement(entity, movement, entityBoundingBox, world, false, collisions);
+        return lithiumCollideMultiAxisMovement(entity, movement, entityBoundingBox, world, collisions);
     }
 
-    private static Vec3d lithiumCollideMultiAxisMovement(@Nullable Entity entity, Vec3d movement, Box entityBoundingBox, World world, boolean getEntityCollisions, List<VoxelShape> otherCollisions) {
+    private static Vec3d lithiumCollideMultiAxisMovement(@Nullable Entity entity, Vec3d movement, Box entityBoundingBox, World world, List<VoxelShape> otherCollisions) {
         //vanilla order: entities, worldborder, blocks. It is unknown whether changing this order changes the result regarding the confusing 1e-7 VoxelShape margin behavior. Not yet investigated
         double velX = movement.x;
         double velY = movement.y;
@@ -77,17 +77,12 @@ public class EntityMixin {
         }
 
         List<VoxelShape> blockCollisions = LithiumEntityCollisions.getBlockCollisions(world, entity, movementSpace);
-        List<VoxelShape> entityWorldBorderCollisions = null;
 
         if (velY != 0.0) {
             velY = VoxelShapes.calculateMaxOffset(Direction.Axis.Y, entityBoundingBox, blockCollisions, velY);
             if (velY != 0.0) {
                 if (!otherCollisions.isEmpty()) {
                     velY = VoxelShapes.calculateMaxOffset(Direction.Axis.Y, entityBoundingBox, otherCollisions, velY);
-                }
-                if (velY != 0.0 && getEntityCollisions) {
-                    entityWorldBorderCollisions = LithiumEntityCollisions.getEntityWorldBorderCollisions(world, entity, movementSpace, entity != null);
-                    velY = VoxelShapes.calculateMaxOffset(Direction.Axis.Y, entityBoundingBox, entityWorldBorderCollisions, velY);
                 }
                 if (velY != 0.0) {
                     entityBoundingBox = entityBoundingBox.offset(0.0, velY, 0.0);
@@ -101,13 +96,6 @@ public class EntityMixin {
                 if (!otherCollisions.isEmpty()) {
                     velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, otherCollisions, velZ);
                 }
-                if (velZ != 0.0 && getEntityCollisions) {
-                    if (entityWorldBorderCollisions == null) {
-                        entityWorldBorderCollisions = LithiumEntityCollisions.getEntityWorldBorderCollisions(world, entity, movementSpace, entity != null);
-                    }
-
-                    velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, entityWorldBorderCollisions, velZ);
-                }
                 if (velZ != 0.0) {
                     entityBoundingBox = entityBoundingBox.offset(0.0, 0.0, velZ);
                 }
@@ -119,13 +107,6 @@ public class EntityMixin {
                 if (!otherCollisions.isEmpty()) {
                     velX = VoxelShapes.calculateMaxOffset(Direction.Axis.X, entityBoundingBox, otherCollisions, velX);
                 }
-                if (velX != 0.0 && getEntityCollisions) {
-                    if (entityWorldBorderCollisions == null) {
-                        entityWorldBorderCollisions = LithiumEntityCollisions.getEntityWorldBorderCollisions(world, entity, movementSpace, entity != null);
-                    }
-
-                    velX = VoxelShapes.calculateMaxOffset(Direction.Axis.X, entityBoundingBox, entityWorldBorderCollisions, velX);
-                }
                 if (velX != 0.0) {
                     entityBoundingBox = entityBoundingBox.offset(velX, 0.0, 0.0);
                 }
@@ -136,13 +117,6 @@ public class EntityMixin {
             if (velZ != 0.0) {
                 if (!otherCollisions.isEmpty()) {
                     velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, otherCollisions, velZ);
-                }
-                if (velZ != 0.0 && getEntityCollisions) {
-                    if (entityWorldBorderCollisions == null) {
-                        entityWorldBorderCollisions = LithiumEntityCollisions.getEntityWorldBorderCollisions(world, entity, movementSpace, entity != null);
-                    }
-
-                    velZ = VoxelShapes.calculateMaxOffset(Direction.Axis.Z, entityBoundingBox, entityWorldBorderCollisions, velZ);
                 }
             }
         }
